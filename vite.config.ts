b5d527160d428c0aaf8a4066c70d083f1f1b37a9
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-05-16 00:50:14 +0100

    Update base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/payment-schedule-simulator/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
